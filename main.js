6d83869d27f356e3f7aaafbe999ed43d113267d2
miarothman <rothman.mia@gmail.com>
2022-09-07 14:08:30 -0400

    Fix operateFunction to take an operator symbol and two numbers and perform the correct mathematical operation. Also, start the creation of calculator GUI

--- main.js
+++ main.js
@@ -21,3 +21,19 @@ function divideFunction(a, b) {
     return a / b;
 }
 console.log(divideFunction(50, 2));
+
+// function for operate
+function operateFunction(operator, a, b) {
+    if(operator === '+') {
+        return addFunction(a, b);
+    } else if(operator === '-') {
+        return minusFunction(a, b);
+    } else if(operator === '/') {
+        return divideFunction(a, b);
+    } else if(operator === '*') {
+        return multiplyFunction(a, b);
+    } else {
+        return "Invalid Operator"
+    }
+};
+console.log(operateFunction("-", 10, 10));
