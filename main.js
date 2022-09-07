7f6ef59f91abf103b2ccb12779a4b4a15a7e657a
miarothman <rothman.mia@gmail.com>
2022-09-07 10:33:01 -0400

    Create functions for add, substract, multiply and divide

---
+++ main.js
@@ -0,0 +1,23 @@
+// function for addition
+function addFunction(a, b) {
+    return a + b;
+}
+console.log(addFunction(5, 5));
+
+// function for subtraction
+function minusFunction(a, b) {
+    return a - b;
+}
+console.log(minusFunction(10, 5));
+
+// function for multiplication
+function multiplyFunction(a, b) {
+    return a * b;
+}
+console.log(multiplyFunction(10, 10));
+
+// function for division
+function divideFunction(a, b) {
+    return a / b;
+}
+console.log(divideFunction(50, 2));
